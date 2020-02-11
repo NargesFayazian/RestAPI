a82735a273386860bf53a5799a3a69c26fe01c2b
nargesfamili <narges.fayazian@gmail.com>
2020-02-10 21:22:28 -0500

    v11

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-//npm install express , morgan(show log),body-parser
+//npm install express , morgan(show log),body-parser,bcrypt
 
 
 
 const express= require('express');
 const app = express();
-const morgan =require('morgan');
+
 const productRoutes=require('./api/routes/products');
 const orderRoutes=require('./api/routes/order');
+const userRoutes= require('./api/routes/user');
+
+const morgan =require('morgan');
 const bodyParser=require('body-parser')
 const mongoose= require('mongoose');
 //show log
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+//   connect db
 mongoose.connect('mongodb://localhost:27017/product', 
 { useNewUrlParser: true,  useUnifiedTopology: true 
 });
 mongoose.Promise=global.Promise;
 
+//=======================================
+
+
+
+
 //rout
 app.use('/products',productRoutes);
 app.use('/order',orderRoutes);
-
+app.use('/user',userRoutes);
 
 
 //error
